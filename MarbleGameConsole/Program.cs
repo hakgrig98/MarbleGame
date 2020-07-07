898f7d2c728b8dd89c24e2e05c214a0da55dd637
Grigor <grigorhakobyanwork@gmail.com>
2020-07-07 16:12:45 +0400

    More work on task.

--- MarbleGameConsole/Program.cs
+++ MarbleGameConsole/Program.cs
@@ -50,7 +50,7 @@ namespace MarbleGameConsole
                 int[] marlbeCordinates = Array.ConvertAll(marbleLine.Trim().Split(' '), (s) => int.Parse(s));
 
                 string holeLine = Console.ReadLine();
-                int[] holeCordinates = Array.ConvertAll(marbleLine.Trim().Split(' '), (s) => int.Parse(s));
+                int[] holeCordinates = Array.ConvertAll(holeLine.Trim().Split(' '), (s) => int.Parse(s));
 
                 _pointMatrix[marlbeCordinates[0], marlbeCordinates[1]] = _pointFactory.Create(_marbleFactory.Create(marlbeCordinates[0], marlbeCordinates[1], _holeFactory.Create(holeCordinates[0], holeCordinates[1])), null);
             }
@@ -83,7 +83,7 @@ namespace MarbleGameConsole
                             var currentMarbleHole = currentMarble.Hole;
                             if (currentMarble.I == currentMarbleHole.I)
                             {
-                                if (IsWall(i))
+                                if (IsWall(i, j, currentMarbleHole.I, currentMarbleHole.J))
                                 {
                                     if (currentMarble.J > currentMarbleHole.J)
                                     {
@@ -101,7 +101,7 @@ namespace MarbleGameConsole
                             }
                             else if (currentMarble.J == currentMarbleHole.J)
                             {
-                                if (IsWall(currentMarble.J))
+                                if (IsWall(i, j, currentMarbleHole.I, currentMarbleHole.J))
                                 {
                                     if (currentMarble.I > currentMarbleHole.I)
                                     {
@@ -135,9 +135,61 @@ namespace MarbleGameConsole
         }
         #endregion
         #region Helpers
-        private static bool IsWall(int i)
+        private static bool IsWall(int i, int j, int k, int z)
         {
-            return false;
+            Position position = Position.E;//Optional
+            if (i == k)
+            {
+                if (j > z)
+                    position = Position.W;
+                else
+                    position = Position.E;
+            }
+            else
+            {
+                if (i > k)
+                    position = Position.N;
+                else
+                    position = Position.S;
+            }
+
+            for (int l = 1; l < _squareCount; l++)
+            {
+                switch (position)
+                {
+                    case Position.E:
+                        if (j + l >= _squareCount)
+                            break;
+
+                        if (_pointMatrix[i, j + l] != null && _pointMatrix[i, j + l].Wall != null)
+                            return false;
+                        break;
+                    case Position.N:
+                        if (i - l < 0)
+                            break;
+
+                        if (_pointMatrix[i - l, j] != null && _pointMatrix[i - l, j].Wall != null)
+                            return false;
+                        break;
+                    case Position.S:
+                        if (i + l >= _squareCount)
+                            break;
+
+                        if (_pointMatrix[i + l, j] != null && _pointMatrix[i + l, j].Wall != null)
+                            return false;
+                        break;
+                    case Position.W:
+                        if (j - l < 0)
+                            break;
+
+                        if (_pointMatrix[i, j - l] != null && _pointMatrix[i, j - l].Wall != null)
+                            return false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return true;
         }
 
         private static void MoveMatrix(Position position)
@@ -149,16 +201,97 @@ namespace MarbleGameConsole
                     if (_pointMatrix[i, j] != null)
                     {
                         var currentMarbleMatrix = _pointMatrix[i, j];
+
+                        if (currentMarbleMatrix != null && currentMarbleMatrix.Marble != null)
+                        {
+
+
+                            switch (position)
+                            {
+                                case Position.E:
+                                    {
+                                        bool isWall = IsWall(i, j, i, _squareCount - 1);
+                                        if (isWall)
+                                        {
+                                            for (int k = j; k < _squareCount; k++)
+                                            {
+                                                if (_pointMatrix[i, k] != null && _pointMatrix[i, k].Wall != null)
+                                                {
+                                                    _pointMatrix[i, k] = _pointMatrix[i, j];
+                                                    _pointMatrix[i, j] = null;
+                                                }
+                                                else
+                                                {
+                                                    _pointMatrix[i, _squareCount-1] = _pointMatrix[i, j];
+                                                    _pointMatrix[i, j] = null;
+                                                }
+                                                    
+                                            }
+                                        }
+                                    }
+                                    break;
+                                case Position.N:
+                                    break;
+                                case Position.S:
+                                    {
+                                        bool isWall = IsWall(i, j, _squareCount-1, j);
+                                        if (isWall)
+                                        {
+                                            for (int k = j; k < _squareCount; k++)
+                                            {
+                                                if (_pointMatrix[i, j] != null && _pointMatrix[i, j].Wall != null)
+                                                {
+                                                    _pointMatrix[_squareCount-1, j] = _pointMatrix[i, j];
+                                                    _pointMatrix[i, j] = null;
+                                                }
+                                                else
+                                                {
+                                                    _pointMatrix[_squareCount-1, j] = _pointMatrix[i, j];
+                                                    _pointMatrix[i, j] = null;
+                                                }
+
+                                            }
+                                        }
+                                    }
+                                    break;
+                                case Position.W:
+                                    {
+                                        bool isWall = IsWall(i, _squareCount - 1, i, j);
+                                        if (isWall)
+                                        {
+                                            for (int k = j; k < _squareCount; k++)
+                                            {
+                                                if (_pointMatrix[i, j-k] != null && _pointMatrix[i, j-k].Wall != null)
+                                                {
+                                                    _pointMatrix[i, j-k] = _pointMatrix[i, j];
+                                                    _pointMatrix[i, j] = null;
+                                                }
+                                                else
+                                                {
+                                                    _pointMatrix[i, j-_squareCount - 1] = _pointMatrix[i, j];
+                                                    _pointMatrix[i, j] = null;
+                                                }
+
+                                            }
+                                        }
+                                    }
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+
+
                     }
                 }
             }
         }
-            #endregion
-            static void Main(string[] args)
-            {
-                ReadInput();
-                Process();
-                PrintOutput();
-            }
+        #endregion
+        static void Main(string[] args)
+        {
+            ReadInput();
+            Process();
+            PrintOutput();
         }
     }
+}
